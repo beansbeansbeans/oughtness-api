02dad8ef7c56ec00d908e7a6db4ee82f415f7e64
Ann Yuan <ayuan@thisisgrow.com>
2015-08-09 07:28:12 -0400

    use correct ahp values

--- utils.js
+++ utils.js
@@ -2,8 +2,29 @@ var Jmat = require('./jmat');
 var ObjectId = require('mongojs').ObjectId;
 var _ = require('underscore');
 
+var preferenceDomain = [0, 0.5];
+var fundamentalScale = [1, 9];
+
+var lerp = function(val) {
+  var a = [preferenceDomain[0], fundamentalScale[0]];
+  var b = [preferenceDomain[1], fundamentalScale[1]];
+  var m = (a[1] - b[1]) / (a[0] - b[0]);
+  var intercept = a[1] - m * a[0];
+  return Math.round(m * val + intercept);
+};
+
 // CORRECT RESULT: https://en.wikipedia.org/wiki/Analytic_hierarchy_process_%E2%80%93_leader_example (by experience)
-// console.log(Jmat.eig(Jmat.Matrix([[1, 0.25, 4], [4, 1, 9], [0.25, 0.11, 1]])).v.e);
+// console.log(Jmat.eig(Jmat.Matrix([
+//   [1, 0.25, 4], 
+//   [4, 1, 9], 
+//   [0.25, 0.11, 1]])).v.e);
+// FAILURE (lots of nans)
+// console.log(Jmat.eig(Jmat.Matrix([
+//   [ 1, 9, 9, 0.11, 9 ],
+//   [ 0.11, 1, 0.11, 1, 1 ],
+//   [ 0.11, 9, 1, 5, 1 ],
+//   [ 9, 1, 0.2, 1, 1 ],
+//   [ 0.11, 1, 1, 1, 1 ]])).v.e);
 
 exports.createVote = function(req, res, client, cb) {
   var cause0id = req.body.causes[0].id;
@@ -79,14 +100,23 @@ exports.getVectors = function(votes, dimensions, causes, fn) {
             return typeof vote.causes[cause._id] !== 'undefined' && typeof vote.causes[nestedCause._id] !== 'undefined';
           });
 
-          result[i].causes[causeIndex][nestedCauseIndex] = 0;
-          result[i].causes[nestedCauseIndex][causeIndex] = 0;
+          result[i].causes[causeIndex][nestedCauseIndex] = 1;
+          result[i].causes[nestedCauseIndex][causeIndex] = 1;
 
           if(matchingVote) {
             sum = matchingVote.causes[cause._id] + matchingVote.causes[nestedCause._id];
             if(sum !== 0) {
-              result[i].causes[causeIndex][nestedCause] = matchingVote.causes[cause._id] / sum;
-              result[i].causes[nestedCauseIndex][causeIndex] = matchingVote.causes[nestedCause._id] / sum;
+              var preference = 1;
+
+              if(matchingVote.causes[cause._id] > matchingVote.causes[nestedCause._id]) {
+                preference = lerp((matchingVote.causes[cause._id] / sum) - 0.5);
+                result[i].causes[causeIndex][nestedCauseIndex] = preference;
+                result[i].causes[nestedCauseIndex][causeIndex] = 1 / preference;
+              } else {
+                preference = lerp((matchingVote.causes[nestedCause._id] / sum) - 0.5);
+                result[i].causes[nestedCauseIndex][causeIndex] = preference;
+                result[i].causes[causeIndex][nestedCauseIndex] = 1 / preference;
+              }
             }
           }
         });
