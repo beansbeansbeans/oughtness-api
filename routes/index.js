313431bc6f940bc2ff76593dab07de89ae5231d9
Ann Yuan <ayuan@thisisgrow.com>
2016-01-20 20:28:24 -0500

    remove

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,6 @@ function Routes (app, ee) {
   app.use(function (req, res, next) {
     console.log("hi", moment(Date.now(), 'x').format('MM-DD-YYYY h:m a'));
     res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers", "Origin", "X-Requested-With, Content-Type", "Accept");
     next();
   });
